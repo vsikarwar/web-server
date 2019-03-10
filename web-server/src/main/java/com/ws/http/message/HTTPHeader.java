0f360585e9b74ed2266ccf7af851a3fda3168c81
Pydev hacker <pydevhacker@gmail.com>
2019-03-10 22:03:56 +0530

    clean-up

--- web-server/src/main/java/com/ws/http/message/HTTPHeader.java
+++ web-server/src/main/java/com/ws/http/message/HTTPHeader.java
@@ -1,11 +1,51 @@
 package com.ws.http.message;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class HTTPHeader {
 	
-	private String key;
+	private String name;
 	private List<String> values;
 	
+	public HTTPHeader(String name) {
+		values = new ArrayList<String>();
+		this.name = name;
+	}
+	
+	public HTTPHeader(String name, String value) {
+		this(name);
+		this.values.add(value);
+	}
+	
+	public HTTPHeader(String name, String[] values) {
+		this(name);
+		Collections.addAll(this.values, values);
+	}
+	
+	public String getValue() {
+		return this.values.get(0);
+	}
+	
+	public List<String> getValues(){
+		return Collections.unmodifiableList(this.values);
+	}
+	
+	public void remove(String value) {
+		this.values.remove(value);
+	}
+	
+	public void add(String value) {
+		this.values.add(value);
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String toString() {
+		return this.name + ": " + String.join(",", this.values);
+	}
 
 }
--- web-server/src/main/java/com/ws/http/message/HTTPMessage.java
+++ web-server/src/main/java/com/ws/http/message/HTTPMessage.java
@@ -1,5 +1,16 @@
 package com.ws.http.message;
 
+import com.viks.http.header.HttpHeaders;
+
 public class HTTPMessage {
+	
+	private HttpHeaders headers;
+	private byte[] content;
+	
+	public HTTPMessage() {
+		headers = new HttpHeaders();
+		content = null;
+	}
+	
 
 }
